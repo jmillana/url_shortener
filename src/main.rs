a570f5596006ba50d952b79529acc7ddac86764a
jmillana <jordi@marimon.tech>
2024-01-19 21:00:22 +0100

    Create a dummy endpoint
    Setup the API routes with warp and tokio

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use md5;
+use warp::Filter;
 pub fn shorten(url: String) -> String {
     let digest = md5::compute(url);
     return format!("{:x}", digest);
 }
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let routes = shorten_url.or(hello);
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await
